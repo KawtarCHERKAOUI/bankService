ad04494ffcefdf379d3e9e82edca431d3690f340
Cherkaoui Kawtar <Kaoutar.Cherkaoui@emsi-edu.ma>
2024-11-07 20:29:11 +0100

    First commit

--- src/main/java/kawtar/emsi/app_2/App2Application.java
+++ src/main/java/kawtar/emsi/app_2/App2Application.java
@@ -1,4 +1,4 @@
-package meryem.emsi.app_2;
+package kawtar.emsi.app_2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/kawtar/emsi/app_2/App2ApplicationTests.java
+++ src/test/java/kawtar/emsi/app_2/App2ApplicationTests.java
@@ -1,4 +1,4 @@
-package meryem.emsi.app_2;
+package kawtar.emsi.app_2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
